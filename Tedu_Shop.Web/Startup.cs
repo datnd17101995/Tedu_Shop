3ab9ddd9dac2c0dc6fa0f36c5011e34cb473c777
Nguyễn Đình Đạt <datnd17101995@gmail.com>
2017-02-11 17:58:23 +0700

    the first comit

---
+++ Tedu_Shop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tedu_Shop.Web.Startup))]
+namespace Tedu_Shop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
